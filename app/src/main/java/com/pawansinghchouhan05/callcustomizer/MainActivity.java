27342d9d5e4f28bf6b789cd975b28972fd402f20
pawan singh chauhan <pawansinghchouhan05@gmail.com>
2016-06-21 10:41:47 +0530

    added activity using annotations support

--- app/src/main/java/com/pawansinghchouhan05/callcustomizer/MainActivity.java
+++ app/src/main/java/com/pawansinghchouhan05/callcustomizer/MainActivity.java
@@ -3,11 +3,16 @@ package com.pawansinghchouhan05.callcustomizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }*/
 }
---
+++ app/src/main/java/com/pawansinghchouhan05/callcustomizer/core/CallCustomizerApplication.java
@@ -0,0 +1,9 @@
+package com.pawansinghchouhan05.callcustomizer.core;
+
+import android.app.Application;
+
+/**
+ * Created by Fitterfox-Pawan on 6/21/2016.
+ */
+public class CallCustomizerApplication extends Application {
+}
