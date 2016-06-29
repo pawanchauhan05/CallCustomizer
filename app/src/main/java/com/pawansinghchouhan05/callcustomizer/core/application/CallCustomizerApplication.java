2b1a9994cf4fbcd608796a26a05484943cf5adec
pawan singh chauhan <pawansinghchouhan05@gmail.com>
2016-06-29 15:34:26 +0530

    added retrofit dependency

--- app/src/main/java/com/pawansinghchouhan05/callcustomizer/core/application/CallCustomizerApplication.java
+++ app/src/main/java/com/pawansinghchouhan05/callcustomizer/core/application/CallCustomizerApplication.java
@@ -4,6 +4,19 @@ import android.app.Application;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.pawansinghchouhan05.callcustomizer.core.utils.Utils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Fitterfox-Pawan on 6/21/2016.
@@ -11,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class CallCustomizerApplication extends Application {
 
     public static DatabaseReference databaseReference;
+    public static Retrofit retrofit;
 
     @Override
     public void onCreate() {
@@ -20,5 +34,31 @@ public class CallCustomizerApplication extends Application {
 
     private void init() {
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request original = chain.request();
+
+                        String token = Utils.readPreferences(getApplicationContext(), "token", "");
+                        if (!token.isEmpty())
+                            return chain.proceed(original.newBuilder()
+                                    .header("X-Auth-Token", token)
+                                    .method(original.method(), original.body())
+                                    .build());
+                        return chain.proceed(original);
+                    }
+                })
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.7/php/CrudExample/index.php/")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
     }
 }
